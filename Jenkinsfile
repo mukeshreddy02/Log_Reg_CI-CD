dc9525ee68fba7bb7d351856de8d42a5834861f0
mukeshreddy02 <mukeshram1995@gmail.com>
2020-01-27 06:43:21 +0000

    deployment testing

---
+++ Jenkinsfile
@@ -0,0 +1,40 @@
+node{
+
+    stage('SCM Checkout'){
+        git url: 'https://github.com/mukeshreddy02/Log_Reg_CI-CD.git',branch: 'master'
+    }
+
+
+    stage('Build Docker Image'){
+        sh 'docker build -t mukeshreddy02/python_flask_log_reg .'
+    }
+
+    stage('Push Docker Image'){
+        withCredentials([string(credentialsId: 'Docker_Hub_Pwd', variable: 'Docker_Hub_Pwd')]) {
+          sh "docker login -u mukeshreddy02 -p ${Docker_Hub_Pwd}"
+        }
+        sh 'docker push mukeshreddy02/python_flask_log_reg'
+     }
+
+    stage('Run Docker Image In Dev Server'){
+
+        def dockerRun = ' docker run  -d -p 5000:5000 --name python_flask_log_reg mukeshreddy02/python_flask_log_reg'
+          
+          sshagent(['DOCKER_SERVER']) {
+          sh 'ssh -o StrictHostKeyChecking=no ubuntu@13.232.172.99 docker stop python_flask_log_reg || true'
+          sh 'ssh  ubuntu@13.232.172.99 docker rm python_flask_log_reg || true'
+          sh 'ssh  ubuntu@13.232.172.99 docker rmi -f  $(docker images -q) || true'
+          sh "ssh  ubuntu@13.232.172.99 ${dockerRun}"
+       }
+       
+    }
+   
+     stage('SendEmailNotification'){
+         emailext body: '''Build is Over
+
+         Regards,
+         Mukesh reddy,
+
+         ''', subject: 'Build is Over', to: 'mukeshram1995@gmail.com'
+      }
+} 
---
+++ app.py
@@ -0,0 +1,50 @@
+# importing the necessary dependencies
+from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS, cross_origin
+import pickle
+
+app = Flask(__name__)  # initializing a flask app
+
+
+@app.route('/', methods=['GET'])  # route to display the home page
+@cross_origin()
+def homePage():
+    return render_template("index.html")
+
+
+@app.route('/predict', methods=['POST', 'GET'])  # route to show the predictions in a web UI
+@cross_origin()
+def index():
+    if request.method == 'POST':
+        try:
+            #  reading the inputs given by the user
+            rate_marriage = float(request.form['rate_marriage'])
+            age = float(request.form['age'])
+            yrs_married = float(request.form['yrs_married'])
+            children = float(request.form['children'])
+            religious = float(request.form['religious'])
+            educ = float(request.form['educ'])
+            occupation = str(request.form['occupation'])
+            occupation_husb = str(request.form['occupation_husb'])
+            filename = 'finalized_model.pickle'
+            loaded_model = pickle.load(open(filename, 'rb'))  # loading the model file from the storage
+            # predictions using the loaded model file
+            prediction = loaded_model.predict(
+                [[1, int(occupation[0]), int(occupation[1]), int(occupation[2]), int(occupation[3]), int(occupation[4]),
+                  int(occupation_husb[0]), int(occupation_husb[1]), int(occupation_husb[2]),
+                  int(occupation_husb[3]), int(occupation_husb[4]),
+                  rate_marriage, age, yrs_married, children, religious, educ]])
+            print('prediction is', prediction)
+            # showing the prediction results in a UI
+            return render_template('results.html', prediction=prediction[0])
+        except Exception as e:
+            print('The Exception message is: ', e)
+            return "something is wrong"
+    # return render_template('results.html')
+    else:
+        return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8001, debug=True)
+# app.run(debug=True) # running the app
